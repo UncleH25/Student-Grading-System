676a07c0049de1aa559db51c34fc4b15057d8c5d
UncleH25 <oneilljarryd@gmail.com>
2025-05-13 07:08:12 +0200

    Added an if-else if-else block to determine the grad letter according to the student's percentage.

--- Student Grading System.cpp
+++ Student Grading System.cpp
@@ -20,6 +20,40 @@ int main()
 
 	//Crash the program if the percentage score is greater than 100.00
 	assert(PERCENTAGE_SCORE > 100.00);
+
+	//Determine the letter grade
+	if (PERCENTAGE_SCORE == 100.00 || PERCENTAGE_SCORE >= 80.00)
+	{
+		gradeLetter = 'A+';
+	}
+	else if (PERCENTAGE_SCORE == 79.00 || PERCENTAGE_SCORE >= 70.00)
+	{
+		gradeLetter = 'A';
+	}
+	else if (PERCENTAGE_SCORE == 69.00 || PERCENTAGE_SCORE >= 60.00)
+	{
+		gradeLetter = 'A-';
+	}
+	else if (PERCENTAGE_SCORE == 59.00 || PERCENTAGE_SCORE >= 50.00)
+	{
+		gradeLetter = 'B';
+	}
+	else if (PERCENTAGE_SCORE == 49.00 || PERCENTAGE_SCORE >= 40.00)
+	{
+		gradeLetter = 'C';
+	}
+	else if (PERCENTAGE_SCORE == 39.00 || PERCENTAGE_SCORE >= 33.00)
+	{
+		gradeLetter = 'D';
+	}
+	else if (PERCENTAGE_SCORE < 32.00)
+	{
+		gradeLetter = 'F';
+	}
+	else
+	{
+		cout << "Invalid percentage score. Please enter a value between 0.0 and 100.00." << endl;
+	}
     
     return 0;
 }
