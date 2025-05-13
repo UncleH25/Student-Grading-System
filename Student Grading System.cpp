36341f6dc2fc35a95205a70a7051e5d229c16389
UncleH25 <oneilljarryd@gmail.com>
2025-05-13 07:02:39 +0200

    Added code to prompt the user to enter the student's name and percentage score between 0.00 and 100.00. Also added an assert line to crash the program if the percentage score is greater than 100.00.

--- Student Grading System.cpp
+++ Student Grading System.cpp
@@ -11,6 +11,15 @@ int main()
 	string studentName;
 	double PERCENTAGE_SCORE;
 	char gradeLetter;
+
+	//Input
+	cout << "Enter the student's name: ";
+	getline(cin, studentName);
+	cout << "Enter the student's percentage score (0.0-100.00): ";
+	cin >> PERCENTAGE_SCORE;
+
+	//Crash the program if the percentage score is greater than 100.00
+	assert(PERCENTAGE_SCORE > 100.00);
     
     return 0;
 }
